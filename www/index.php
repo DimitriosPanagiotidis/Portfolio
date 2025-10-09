e665f4d5832ddb684825560f7f08bb9e6aac2440
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2025-10-09 11:53:25 +0200

    Index page started up and navbar too

---
+++ www/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require 'database.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dimitris Panagiotidis</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="home">
+        <?php include 'navbar.php' ?>    
+
+
+        <h1>Sup! My name is Dimitrios Panagiotidis</h1>
+        <h2>Software Developer wannabe...</h2>
+    </header>
+
+<section id="about">
+    <article>
+        <h2>About Me</h2>
+        <p>Hey! I am Dimitris and I am 22 years old.</p>
+
+    <ul>
+        <li>I have been working/studying as a cook for 4 years straight from 2019 until 2023.</li>
+        <li>I am looking forward to becoming a Software Developer.</li>
+        <li>At the moment, I am educating myself at Nova College in Haarlem.</li>
+        <li>I am self-taught in building computers — in my spare time, I’m usually around technology.</li>
+        <li>I speak 3 languages.</li>
+    </ul>
+</article>
+
+
+</section>
+
+
+
+<footer>
+    <?php include 'footer.php' ?>
+</footer>
+</body>
+</html>
