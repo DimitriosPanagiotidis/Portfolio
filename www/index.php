02d2f4501175bd0fd7e6718fbd074fcc065769c0
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2025-10-14 11:50:29 +0200

    index database connection

--- www/index.php
+++ www/index.php
@@ -2,47 +2,90 @@
 
 require 'database.php';
 
+$sql = "SELECT * FROM educations";
+$result = mysqli_query($conn, $sql);
+$educations = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dimitris Panagiotidis</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="reset.css">
 </head>
+
 <body>
     <header id="home">
-        <?php include 'navbar.php' ?>    
-
-
-        <h1>Sup! My name is Dimitrios Panagiotidis</h1>
-        <h2>Software Developer wannabe...</h2>
+        <!-- NAVBAR -->
+        <?php include 'navbar.php' ?>
+        <!-- END OF NAVBAR -->
+        <div class="intro">
+            <h1>Sup! My name is Dimitrios Panagiotidis</h1>
+            <h2>Software Developer wannabe...</h2>
+            <img src="./images/Dimitris_cartoon.png" alt="Dimitris Panagiotidis">
+        </div>
     </header>
+    <main>
+        <!-- BEGIN OF ABOUT ME  -->
+        <section id="about">
+            <article>
+                <h2>About Me</h2>
+                <p>My name is Dimitris and I am 22 years old.</p>
+                <ul>
+                    <li>I am a Software Developer in progression.</li>
+                    <li>At the moment, I am educating myself at Nova College in Haarlem.</li>
+                    <li>I have been working/studying as a cook for 4 years straight from 2019 until 2023.</li>
+                    <li>I am self-taught in building computers — in my spare time, I am usually around technology.</li>
+                    <li>Dutch | English | Greek</li>
+                </ul>
+            </article>
+        </section>
+        <!-- END OF ABOUT ME -->
 
-<section id="about">
-    <article>
-        <h2>About Me</h2>
-        <p>Hey! I am Dimitris and I am 22 years old.</p>
+        <!-- BEGIN OF EDUCATION -->
+        <section id="education">
+            <h2>Education</h2>
+            <table>
+                <caption>Educational background</caption>
+                <tr>
+                    <th scope="col">Year</th>
+                    <th scope="col">Institution</th>
+                    <th scope="col">Degree</th>
+                </tr>
+                <tr>
+                    <td>2023 - Present</td>
+                    <td>Nova College, Haarlem</td>
+                    <td>Software Developer (MBO-4)</td>
+                </tr>
+                <tr>
+                    <td>2017 - 2019</td>
+                    <td>ROC van Amsterdam, Amsterdam</td>
+                    <td>Cook (MBO-3)</td>
+                </tr>
+                <tr>
+                    <td>2011 - 2017</td>
+                    <td>Montessori Lyceum, Amsterdam</td>
+                    <td>High School Diploma (HAVO)</td>
+                </tr>
+            </table>
+        </section>
 
-    <ul>
-        <li>I have been working/studying as a cook for 4 years straight from 2019 until 2023.</li>
-        <li>I am looking forward to becoming a Software Developer.</li>
-        <li>At the moment, I am educating myself at Nova College in Haarlem.</li>
-        <li>I am self-taught in building computers — in my spare time, I’m usually around technology.</li>
-        <li>I speak 3 languages.</li>
-    </ul>
-</article>
-
-
-</section>
+        <!-- END OF EDUCATION -->
 
 
 
-<footer>
-    <?php include 'footer.php' ?>
-</footer>
+    </main>
+
+    <footer>
+        <?php include 'footer.php' ?>
+    </footer>
 </body>
+
 </html>
